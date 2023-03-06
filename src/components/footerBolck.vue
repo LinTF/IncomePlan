1510ef26d7134d0e7b28c45a83832dfa9b77e4bf
Iris <lin.tf1124@gmail.com>
2023-03-07 01:58:31 +0800

    完成收入區塊

---
+++ src/components/footerBolck.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="footer">
+        {{ copyright }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FooterBlock',
+        data() {
+            return {
+                copyright: '© 2023 Ting Fang Lin',
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    $black-color: #171717;
+    $white-color: #f3f3f3;
+
+    #footer {
+        background-color: $black-color;
+        text-align: center;
+        color: $white-color;
+    }
+</style>
--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,18 +1,75 @@
 <template>
-    <home />
+    <header></header>
+
+    <main>
+        <h1 class="txt-center">收入支出分析</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div id="income">
+                        <h3>實際收入</h3>
+                        <p class="txt-right">剩餘 {{ incomeTotal }} </p>
+                        <hr />
+                        <p>本薪 <input type="text" v-model="income" @keyup="total" /></p>
+                        <p>勞保 <input type="text" v-model="laborInsurance" @keyup="total" /></p>
+                        <p>健保 <input type="text" v-model="healthInsurance" @keyup="total" /></p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div id="outlay">
+                        <h3>固定支出</h3>
+                        <p class="txt-right">總支出 {{  }}</p>
+                        <hr />
+                        <p>生活費 <input type="text" /></p>
+                        <p>孝親費 <input type="text" /></p>
+                        <p>保險 <input type="text" /></p>
+                        <p>學貸 <input type="text" /></p>
+                        <p>電話費 <input type="text" /></p>
+                        <p>其他花費 <input type="text" /></p>
+                        <p>交通費 <input type="text" /></p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    
+                </div>
+                <div class="col-md-3">
+                    
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <footerBlock />
+    </footer>
 </template>
 
 <script>
-    import home from '@/components/home.vue';
-    
+    import footerBlock from '@/components/footerBolck.vue'
+
     export default {
         name: 'App',
         components: {
-            home
+            footerBlock
+        },
+        data() {
+            return {
+                income: 0,
+                laborInsurance: 0,
+                healthInsurance: 0,
+                incomeTotal: 0,
+            }
+        },
+        computed: {
+            total: function () {
+                this.incomeTotal = parseInt(this.income) - parseInt(this.laborInsurance) - parseInt(this.healthInsurance)
+            }
         }
     };
 </script>
 
-<style>
-
+<style scoped lang="scss">
+    h1 {
+        margin-top: 20px;
+    }
 </style>
