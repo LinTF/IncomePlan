1a080c95624220333ac64189ea2166b73aa5de66
Iris <lin.tf1124@gmail.com>
2023-03-12 04:52:38 +0800

    新增儲蓄投資項目元件

---
+++ src/components/saveBlock.vue
@@ -0,0 +1,93 @@
+<template>
+    <div id="save-item">
+        <h4>儲蓄/投資</h4>
+        <hr />
+        <p class="txt-right">總儲蓄 {{ saveTotal }}</p>
+        <div>
+            <div class="income-item">
+                <div class="row">
+                    <div class="col-6 title">
+                        <span class="txt-center">項目</span>
+                    </div>
+                    <div class="col-6 title">
+                        <span class="txt-center">金額</span>
+                    </div>
+                </div>
+            </div>
+            <div class="income-item">
+                <div class="row">
+                    <div class="col-3 title">
+                        <span>保險</span>
+                    </div>
+                    <div class="col-9">
+                        <input type="text" v-model="saveInsurance" @keyup="saveAll" />
+                    </div>
+                </div>
+            </div>
+            <div class="income-item">
+                <div class="row">
+                    <div class="col-3 title">
+                        <span>儲蓄險</span>
+                    </div>
+                    <div class="col-9">
+                        <input type="text" v-model="savings" @keyup="saveAll" />
+                    </div>
+                </div>
+            </div>
+            <div class="income-item">
+                <div class="row">
+                    <div class="col-3 title">
+                        <span>基金</span>
+                    </div>
+                    <div class="col-9">
+                        <input type="text" v-model="foundation" @keyup="saveAll" />
+                    </div>
+                </div>
+            </div>
+            <div class="income-item">
+                <div class="row">
+                    <div class="col-3 title">
+                        <span>活儲</span>
+                    </div>
+                    <div class="col-9">
+                        <input type="text" v-model="bankMoney" @keyup="saveAll" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SaveItems",
+        data() {
+            return {
+                // 儲蓄/投資
+                /// 總儲蓄
+                saveTotal: 0,
+                /// 保險
+                saveInsurance: 0,
+                /// 儲蓄險
+                savings: 0,
+                /// 基金
+                foundation: 0,
+                /// 活儲
+                bankMoney: 0,
+            }
+        },
+        computed: {
+            saveAll: function () {
+                this.saveTotal = parseInt(this.saveInsurance) + parseInt(this.savings) + parseInt(this.foundation) + parseInt(this.bankMoney);
+
+                if (isNaN(this.saveTotal)) {
+                    this.saveTotal = 0
+                }
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
--- src/pages/App.vue
+++ src/pages/App.vue
@@ -18,63 +18,7 @@
                     <planBlock :saveMoney=saveTotal />
                 </div>
                 <div class="col-md-6">
-                    <div id="save-plan">
-                        <h4>儲蓄/投資</h4>
-                        <hr />
-                        <p class="txt-right">總儲蓄 {{ saveTotal }}</p>
-                        <div>
-                            <div class="income-item">
-                                <div class="row">
-                                    <div class="col-6 title">
-                                        <span class="txt-center">項目</span>
-                                    </div>
-                                    <div class="col-6 title">
-                                        <span class="txt-center">金額</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="income-item">
-                                <div class="row">
-                                    <div class="col-3 title">
-                                        <span>保險</span>
-                                    </div>
-                                    <div class="col-9">
-                                        <input type="text" v-model="saveInsurance" @keyup="saveAll" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="income-item">
-                                <div class="row">
-                                    <div class="col-3 title">
-                                        <span>儲蓄險</span>
-                                    </div>
-                                    <div class="col-9">
-                                        <input type="text" v-model="savings" @keyup="saveAll" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="income-item">
-                                <div class="row">
-                                    <div class="col-3 title">
-                                        <span>基金</span>
-                                    </div>
-                                    <div class="col-9">
-                                        <input type="text" v-model="foundation" @keyup="saveAll" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="income-item">
-                                <div class="row">
-                                    <div class="col-3 title">
-                                        <span>活儲</span>
-                                    </div>
-                                    <div class="col-9">
-                                        <input type="text" v-model="bankMoney" @keyup="saveAll" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <saveBlock />
                 </div>
             </div>
         </div>
@@ -91,6 +35,7 @@
     import outlayBlock from '@/components/outlayBlock.vue'
     import budgetBlock from '@/components/budgetBlock.vue'
     import planBlock from '@/components/planBlock.vue'
+    import saveBlock from '@/components/saveBlock.vue'
 
     export default {
         name: 'App',
@@ -99,7 +44,8 @@
             incomeBlock,
             outlayBlock,
             budgetBlock,
-            planBlock
+            planBlock,
+            saveBlock
         },
         data() {
             return {
@@ -107,26 +53,10 @@
                 income: 0,
                 // 存錢總額（30%）
                 saveTotal: 0,
-
-                // 儲蓄/投資
-                /// 總儲蓄
-                saveTotal: 0,
-                /// 保險
-                saveInsurance: 0,
-                /// 儲蓄險
-                savings: 0,
-                /// 基金
-                foundation: 0,
-                /// 活儲
-                bankMoney: 0,
-
-                
             }
         },
         computed: {
-            saveAll: function () {
-                // this.saveTotal = parseInt(this.saveInsurance) + parseInt(this.savings) + parseInt(this.foundation) + parseInt(this.bankMoney)
-            }
+
         },
         methods: {
             // 傳入budgetBlock > props:incomeTotal(總收入)
