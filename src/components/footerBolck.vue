657e4b266725162b94e4ea15c90949f9d8932527
Iris <lin.tf1124@gmail.com>
2023-03-29 19:33:57 +0800

    加上日期

--- src/components/footerBolck.vue
+++ src/components/footerBolck.vue
@@ -9,7 +9,7 @@
         name: 'FooterBlock',
         data() {
             return {
-                copyright: '© 2023 Ting Fang Lin',
+                copyright: '© 2023 Ting Fang Lin @329',
             }
         }
     };
